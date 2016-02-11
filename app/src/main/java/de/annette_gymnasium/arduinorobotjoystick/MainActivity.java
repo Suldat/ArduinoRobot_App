85281cdb5a9ec6e43c42fd5e09d34d6a8c7e3934
Suldat <Suldat@users.noreply.github.com>
2016-02-11 20:08:02 +0100

    Überschreiben von Methoden während eines Lebenszykluses von der App

--- app/src/main/java/de/annette_gymnasium/arduinorobotjoystick/MainActivity.java
+++ app/src/main/java/de/annette_gymnasium/arduinorobotjoystick/MainActivity.java
@@ -29,7 +29,6 @@ public class MainActivity extends AppCompatActivity {
 
     Button btnBluetooth;
 
-    boolean nimmtAuf = false;
     //BluetoothAdapter bluetooth = BluetoothAdapter.getDefaultAdapter();
     private static final int REQUEST_ENABLE_BT = 1;
     private BluetoothAdapter btAdapter = null;
@@ -41,7 +40,7 @@ public class MainActivity extends AppCompatActivity {
             UUID.fromString("00001101-0000-1000-8000-00805F9B34FB");
 
     // Insert your server's MAC address
-    private static String address = "00:00:00:00:00:00";
+    private static String address = "BC:85:56:31:06:24";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -120,6 +119,73 @@ public class MainActivity extends AppCompatActivity {
                 },1000);
             }
         });
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+
+        Log.d(TAG, "...In onResume - Attempting client connect...");
+
+        // Set up a pointer to the remote node using it's address.
+        BluetoothDevice device = btAdapter.getRemoteDevice(address);
+
+        // Two things are needed to make a connection:
+        //   A MAC address, which we got above.
+        //   A Service ID or UUID.  In this case we are using the
+        //     UUID for SPP.
+        try {
+            btSocket = device.createRfcommSocketToServiceRecord(MY_UUID);
+        } catch (IOException e) {
+            errorExit("Fatal Error", "In onResume() and socket create failed: " + e.getMessage() + ".");
+        }
+
+        // Discovery is resource intensive.  Make sure it isn't going on
+        // when you attempt to connect and pass your message.
+        btAdapter.cancelDiscovery();
+
+        // Establish the connection.  This will block until it connects.
+        Log.d(TAG, "...Connecting to Remote...");
+        try {
+            btSocket.connect();
+            Log.d(TAG, "...Connection established and data link opened...");
+        } catch (IOException e) {
+            try {
+                btSocket.close();
+            } catch (IOException e2) {
+                errorExit("Fatal Error", "In onResume() and unable to close socket during connection failure" + e2.getMessage() + ".");
+            }
+        }
+
+        // Create a data stream so we can talk to server.
+        Log.d(TAG, "...Creating Socket...");
+
+        try {
+            outStream = btSocket.getOutputStream();
+        } catch (IOException e) {
+            errorExit("Fatal Error", "In onResume() and output stream creation failed:" + e.getMessage() + ".");
+        }
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+
+        Log.d(TAG, "...In onPause()...");
+
+        if (outStream != null) {
+            try {
+                outStream.flush();
+            } catch (IOException e) {
+                errorExit("Fatal Error", "In onPause() and failed to flush output stream: " + e.getMessage() + ".");
+            }
+        }
+
+        try     {
+            btSocket.close();
+        } catch (IOException e2) {
+            errorExit("Fatal Error", "In onPause() and failed to close socket." + e2.getMessage() + ".");
+        }
     }
 
     void makroAufnehmen() {
