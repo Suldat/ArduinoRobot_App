6a23417f847c074bacbf434458e418f3fb0444cf
Suldat <Suldat@users.noreply.github.com>
2016-02-11 19:43:35 +0100

    Bluetooth-Funktionalität hinzugefügt

--- app/src/main/java/de/annette_gymnasium/arduinorobotjoystick/MainActivity.java
+++ app/src/main/java/de/annette_gymnasium/arduinorobotjoystick/MainActivity.java
@@ -3,11 +3,13 @@ package de.annette_gymnasium.arduinorobotjoystick;
 import android.app.AlertDialog;
 import android.bluetooth.BluetoothAdapter;
 import android.content.DialogInterface;
+import android.content.Intent;
 import android.graphics.Color;
 import android.graphics.drawable.GradientDrawable;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -17,13 +19,35 @@ import android.widget.Toast;
 import com.jmedeisis.bugstick.Joystick;
 import com.jmedeisis.bugstick.JoystickListener;
 
+import java.io.IOException;
+import java.io.OutputStream;
+import java.util.UUID;
+
 public class MainActivity extends AppCompatActivity {
 
+    private static final String TAG = "MainActivityClass";
+
+    Button btnBluetooth;
+
     boolean nimmtAuf = false;
-    BluetoothAdapter bluetooth = BluetoothAdapter.getDefaultAdapter();
+    //BluetoothAdapter bluetooth = BluetoothAdapter.getDefaultAdapter();
+    private static final int REQUEST_ENABLE_BT = 1;
+    private BluetoothAdapter btAdapter = null;
+    private BluetoothSocket btSocket = null;
+    private OutputStream outStream = null;
+
+    // Well known SPP UUID
+    private static final UUID MY_UUID =
+            UUID.fromString("00001101-0000-1000-8000-00805F9B34FB");
+
+    // Insert your server's MAC address
+    private static String address = "00:00:00:00:00:00";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+        Log.d(TAG, "In onCreate()");
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
@@ -33,7 +57,12 @@ public class MainActivity extends AppCompatActivity {
         Button buttonAufnehmen = (Button) findViewById(R.id.buttonAufnehmen);
         final Button buttonStick = (Button) findViewById(R.id.buttonStick);
         final GradientDrawable bgShape = (GradientDrawable) buttonStick.getBackground().getCurrent();
+        btnBluetooth = (Button) findViewById(R.id.buttonBluetooth);
 
+        btAdapter = BluetoothAdapter.getDefaultAdapter();
+        checkBTState();
+
+        /*
         String status = "";
 
         if(bluetooth == null) {
@@ -57,7 +86,7 @@ public class MainActivity extends AppCompatActivity {
         }
 
         Toast.makeText(this, status, Toast.LENGTH_LONG).show();
-
+        */
         //der Listener vom Joystick
         joystick.setJoystickListener(new JoystickListener() {
             @Override
@@ -68,6 +97,7 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onDrag(float degrees, float offset) {
                 textView.setText("onDrag. degrees: " +degrees +"; offset: " +offset);
+                sendData(degrees +";" +offset);
             }
 
             @Override
@@ -94,5 +124,46 @@ public class MainActivity extends AppCompatActivity {
 
     void makroAufnehmen() {
 
+    }
+
+    private void errorExit(String title, String message){
+        Toast msg = Toast.makeText(getBaseContext(),
+                title + " - " + message, Toast.LENGTH_SHORT);
+        msg.show();
+        finish();
+    }
+
+    private void checkBTState() {
+        // Check for Bluetooth support and then check to make sure it is turned on
+
+        // Emulator doesn't support Bluetooth and will return null
+        if(btAdapter==null) {
+            errorExit("Schwerer Fehler", "Bluetooth wird nicht unterstützt. App wird beendet");
+        } else {
+            if (btAdapter.isEnabled()) {
+                Log.d(TAG, "...Bluetooth ist eingeschaltet...");
+            } else {
+                //Prompt user to turn on Bluetooth
+                Intent enableBtIntent = new Intent(btAdapter.ACTION_REQUEST_ENABLE);
+                startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT);
+            }
+        }
+    }
+
+    private void sendData(String message) {
+        byte[] msgBuffer = message.getBytes();
+
+        Log.d(TAG, "...Daten werden gesendet: " + message + "...");
+
+        try {
+            outStream.write(msgBuffer);
+        } catch (IOException e) {
+            String msg = "In onResume() and an exception occurred during write: " + e.getMessage();
+            if (address.equals("00:00:00:00:00:00"))
+                msg = msg + ".\n\nUpdate your server address from 00:00:00:00:00:00 to the correct address on line 37 in the java code";
+            msg = msg +  ".\n\nCheck that the SPP UUID: " + MY_UUID.toString() + " exists on server.\n\n";
+
+            errorExit("Fatal Error", msg);
+        }
     }
 }
